56bc37d5965114bc91fb4e1a7c4bbdeb75e2bbcd
irakli.tavberidze <irakli.tavberidze@webfg.com>
2021-11-25 23:29:42 +0100

    starter

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,13 @@
-import React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <div className="text-component">
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <div className="max-width-xxxs margin-y-sm">
-      <StaticImage
-        src="../images/gatsby-astronaut.png"
-        width={300}
-        quality={95}
-        formats={["AUTO", "WEBP", "AVIF"]}
-        alt="A Gatsby astronaut"
-        style={{ marginBottom: `1.45rem` }}
-      />
-      </div>
-      <p>
-        <Link to="/page-2/">Go to page 2</Link>
-      </p>
-      <p>
-        <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-      </p>
-    </div>
-  </Layout>
-)
+import React from "react";
+import Header from "../components/Header/Header";
+import { useEffect } from "react";
+const IndexPage = () => {
+  useEffect(() => {
+    document.getElementsByTagName("html")[0].className += " js";
+  }, []);
+  return (
+    <Header />
+  )
+}
 
 export default IndexPage
